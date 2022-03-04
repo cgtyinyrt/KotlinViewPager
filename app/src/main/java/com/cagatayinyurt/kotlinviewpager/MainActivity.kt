7edfaf076c71a21ef29f4371d273564bec613aa0
Çağatay İnyurt <cagatayinyurt@outlook.com>
2022-03-04 17:05:55 +0300

    Update.

--- app/src/main/java/com/cagatayinyurt/kotlinviewpager/MainActivity.kt
+++ app/src/main/java/com/cagatayinyurt/kotlinviewpager/MainActivity.kt
@@ -6,18 +6,8 @@ import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewPager: ViewPager2
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onBackPressed() {
-        if (viewPager.currentItem == 0) {
-            super.onBackPressed()
-        } else {
-            viewPager.currentItem = viewPager.currentItem - 1
-        }
     }
 }
